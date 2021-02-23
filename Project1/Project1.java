c2e82ba95e70b759c33735652e97ed7c8ae1c332
Abhishek Sinha <cu.18bcs1637@gmail.com>
2021-02-23 14:37:33 +0530

    Create Project1.java

---
+++ Project1/Project1.java
@@ -0,0 +1,15 @@
+Given the following table containing information about employees of an organization, develop a small java application, which accepts employee id from the command prompt and displays the following details as output:
+
+Emp No Emp Name Department Designation and Salary
+
+You may assume that the array is initialized with the following details:
+
+https://user-images.githubusercontent.com/55397457/108821653-f0a05f00-75e3-11eb-9bc1-3eca19342f2e.jpg
+
+Salary is calculated as Basic+HRA+DA-IT. (DA details are given in the Designation table)
+
+Designation details :
+
+https://user-images.githubusercontent.com/55397457/108822104-9ce24580-75e4-11eb-86b8-eaa480c87068.jpg
+
+Use Switch-Case to print Designation in the output and to find the value of DA for a particular employee.
